c63d4019f76b0923d6567558c288d3efa527bcd2
Nasir Abbas <nasir000376@gmail.com>
2021-01-06 20:14:34 +0500

    feat: Start express server and setup debug

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { json, urlencoded } from 'body-parser';
+import Debug from 'debug';
+import express from 'express';
+
+import { connectDB } from './config/db';
+
+// import { routes } from './routes';
+
+const debug: Debug.IDebugger = Debug('notification:app');
+
+const PORT = parseInt(<string>process.env.PORT) || 5454;
+const app: express.Application = express();
+// Database connection
+// connectDB()
+//     .catch((error: Error) => debug('Error while db connection', error.message));
+
+// Middlewares
+app.use(json());
+app.use(urlencoded({ extended: false }));
+// Use routes as middlewares
+// routes(app);
+
+app.listen(PORT, () => debug(`Server is listening on ${PORT}`));
