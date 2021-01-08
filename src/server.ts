3a414f371130298c70f0de399152d417c139aa23
Nasir Abbas <nasir000376@gmail.com>
2021-01-08 19:01:51 +0500

    feat: Add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,37 @@
-import { json, urlencoded } from 'body-parser';
 import Debug from 'debug';
+
 import express from 'express';
+import 'express-async-errors';
 
 import * as dotenv from 'dotenv';
+import { json, urlencoded } from 'body-parser';
+import { connectDB } from './config/db';
+// import { routes } from './routes';
+import { NotFoundError } from './error';
+import { errorHandler } from './middlewares/error.handler';
 
 dotenv.config();
-
-import { connectDB } from './config/db';
-
-// import { routes } from './routes';
 
 const debug: Debug.IDebugger = Debug('notification:app');
 
 const PORT = parseInt(<string>process.env.PORT) || 5454;
 const app: express.Application = express();
 // Database connection
-// connectDB()
-//     .catch((error: Error) => debug('Error while db connection', error.message));
+connectDB()
+    .catch((error: Error) => debug('Error while db connection', error.message));
 
 // Middlewares
 app.use(json());
 app.use(urlencoded({ extended: false }));
 // Use routes as middlewares
 // routes(app);
+
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError('Route not found.');
+});
+
+app.use(errorHandler);
+
 
 app.listen(PORT, () => debug(`Server is listening on ${PORT}`));
